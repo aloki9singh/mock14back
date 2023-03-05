3d63d2cd76a590ad45f8f03e40af979f804fc278
ALOK SINGH <107742899+aloki9singh@users.noreply.github.com>
2023-03-05 12:56:47 +0530

    bUG APP bACKEND

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const express = require("express");
+const cors = require("cors");
 const { connection } = require("./config/db");
-const app = express();
 
+const { authenticate } = require("./Middleware/authenticate.middleware");
+const { router } = require("./routes/Bug.router");
+const { userRouter } = require("./routes/User.route");
+
+const app = express();
 require("dotenv").config();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Bug App");
 });
 
+app.use("/api", userRouter);
+app.use(authenticate);
+app.use("/api",router)
 app.listen(process.env.PORT, async (req, res) => {
-    try {
-      await connection;
-      console.log("Server Connected to database");
-    } catch (err) {
-      console.log({ err: err });
-    }
-    console.log(`Server Running On Port ${process.env.PORT}`);
-  });
-  
+  try {
+    await connection;
+    console.log("Server Connected to database");
+  } catch (err) {
+    console.log({ err: err });
+  }
+  console.log(`Server Running On Port ${process.env.PORT}`);
+});
